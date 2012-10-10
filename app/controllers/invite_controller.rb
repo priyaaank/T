3b98d277d5f1c121993acbea9b4874047e17a2cc
Priyank Gupta <priyaaank@gmail.com>
2012-10-10 11:05:15 +0530

    Changed sym based param keys to be string

--- app/controllers/invite_controller.rb
+++ app/controllers/invite_controller.rb
@@ -3,7 +3,7 @@ class InviteController < ApplicationController
   skip_filter :authenticate_user!
 
   def create
-    invitation_request = Invite.new(:email => params[:invite][:email])
+    invitation_request = Invite.new(:email => params["invite"]["email"])
     invitation_request.save! if invitation_request.valid?
     render :text => "", :head => :ok
   end
